64f05e4b1a72dd3fe354092b88fb8d2bedae64fe
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-17 19:13:28 -0400

    Adds login middleware to catch bad attempts

--- middleware/users.js
+++ middleware/users.js
@@ -1,36 +1,48 @@
 const Users = require('models/queries/users.js');
 
 module.exports = {
-  verifyAccountInfo
+  verifyAccountInfo,
+  verifyLogin
 };
 
-async function verifyAccountInfo(req, res, next) {
+async function verifyNewAccount(req, res, next) {
   const { email, password, nickname } = req.body;
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: 'Email & password are required for this action.' });
+  if (!email || !password || !nickname) {
+    // Could expand to add minimum character length, etc
+    // For this tiny example app, I'm not too concerned about it
+    return res.status(400).json({
+      message: 'Email, password, and nickname are required for this action.'
+    });
   }
 
-  if (nickname) {
-    // User is registering an account
-    const emailExists = await Users.find({ email }).first();
-    const nicknameExists = await Users.find({ nickname }).first();
+  const emailExists = await Users.find({ email }).first();
+  const nicknameExists = await Users.find({ nickname }).first();
 
-    if (emailExists) {
-      return res.status(400).json({
-        route: 'account/create',
-        message: 'An account with that email address already exists.'
-      });
-    }
+  if (emailExists) {
+    return res.status(400).json({
+      route: 'account/create',
+      message: 'An account with that email address already exists.'
+    });
+  }
 
-    if (nicknameExists) {
-      return res.status(400).json({
-        route: 'account/create',
-        message: 'That nickname is already taken.'
-      });
-    }
+  if (nicknameExists) {
+    return res.status(400).json({
+      route: 'account/create',
+      message: 'That nickname is already taken.'
+    });
+  }
+
+  next();
+}
+
+function verifyLogin(req, res, next) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: 'Email and password are required to login'
+    });
   }
 
   next();
