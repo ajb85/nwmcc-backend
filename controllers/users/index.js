68d16f3209b4e246c964b5a7d2f4c88049a9bf27
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-17 23:41:03 -0400

    Creates login/register routes

---
+++ controllers/users/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const generateToken = require('config/generateToken.js');
+const { verifyNewAccount, verifyLogin } = require('middleware/users.js');
+
+const Users = require('models/queries/users.js');
+
+router.post('/register', verifyNewAccount, async (req, res) => {
+  const { email, password, nickname } = req.body;
+
+  const newUser = await Users.create({ email, password, nickname });
+
+  if (newUser) {
+    const token = generateToken(newUser);
+    return res.status(201).json({ ...newUser, token });
+  }
+});
+
+router.post('/login', verifyLogin, async (req, res) => {
+  const token = generateToken(res.locals.user);
+  return res.status(200).json({ ...res.locals.user, token });
+});
