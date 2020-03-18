ce4cee0e1a4c9ac157782336f06e34cc484d0bf7
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 18:10:17 -0400

    Adds routes to get chats and their messages as well as post them

---
+++ controllers/chats/index.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+
+const Chats = require('models/queries/chats.js');
+const Messages = require('models/queries/messages.js');
+
+const {
+  verifyUserInChat,
+  verifyUserNotInChat
+} = require('middleware/chats.js');
+
+router.get('/byUser', async (req, res) => {
+  const { id } = res.locals.user;
+  const userChats = await Chats.find({ 'u.id': id });
+
+  return res.status(200).json(userChats);
+});
+
+router.get('/:chat_id/messages', verifyUserInChat, async (req, res) => {
+  const { chat_id } = req.params;
+  const messages = await Messages.find({ 'c.id': chat_id });
+
+  // The column-reverse used on the frontend means it's best to display
+  // the messages in reverse order, thus I reverse before sending back
+  return res
+    .status(200)
+    .json({ messages: messages.reverse(), limit: process.env.CHAT_MSG_LIMIT });
+});
+
+router.post('/:chat_id/join', verifyUserNotInChat, async (req, res) => {
+  const { chat_id } = req.params;
+  const { id: user_id } = res.locals.user;
+
+  const newChat = await Chats.join(user_id, chat_id);
+
+  return res.status(201).json(newChat);
+});
+
+router.post('/:chat_id/message', async (req, res) => {
+  const { content } = req.body;
+  const { id: user_id } = res.locals.user;
+  const { chat_id } = req.params;
+
+  const newMessage = await Messages.create({ content, chat_id, user_id });
+
+  return res.sendStatus(201);
+});
+
+module.exports = router;
