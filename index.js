aa5b59bd1c3e6f317dffcdd4c97cb0f046678461
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-19 02:53:55 -0400

    Cosmetic changes to code

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('express-async-errors');
 require('app-module-path').addPath(__dirname);
 
 require('api/api.js');
+
 const http = require('api/http.js');
 http.listen((port = process.env.PORT || 4500), () =>
   console.log(`\n** Running on port ${port} **\n`)
